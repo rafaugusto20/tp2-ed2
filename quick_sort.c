f22c6fe16c6ccd3f23ec5966b810364e594e1cd3
Rafael Augusto <rafael.afo@aluno.ufop.edu.br>
2021-12-06 18:29:49 -0300

    Adicionei os arquivos do quicksort externo

---
+++ quick_sort.c
@@ -0,0 +1,189 @@
+#include <stdio.h>
+#include <limits.h>
+
+#include "quick_sort.h"
+
+
+void QuicksortExterno(FILE **ArqLi, FILE **ArqEi, FILE **ArqLEs, int Esq, int Dir){
+    int i, j;
+
+    TipoArea Area;
+
+    if((Dir - Esq) < 1){
+        return;
+    }
+
+    FAVazia(&Area);
+
+    Particao(ArqLi, ArqEi, ArqLEs, Area, Esq, Dir, &i, &j);
+
+    if((i - Esq) < (Dir - j)){
+        QuicksortExterno(ArqLi, ArqEi, ArqLEs, Esq, i);
+        QuicksortExterno(ArqLi, ArqEi, ArqLEs, j, Dir);
+    }else{
+        QuicksortExterno(ArqLi, ArqEi, ArqLEs, j, Dir);
+        QuicksortExterno(ArqLi, ArqEi, ArqLEs, Esq, i);
+    }
+}
+
+void LeSup(FILE **ArqLEs, Aluno *UltLido, int *Ls, short *OndeLer){
+    fseek(*ArqLEs, (*Ls - 1) * sizeof(Aluno), SEEK_SET);
+    fread(UltLido, sizeof(Aluno), 1, *ArqLEs);
+    (*Ls)--;
+    *OndeLer = FALSE;
+}
+
+void LeInf(FILE **ArqLi, Aluno *UltLido, int *Li, short *OndeLer){
+    fread(UltLido, sizeof(Aluno), 1, *ArqLi);
+    (*Li)++;
+    *OndeLer = TRUE;
+}
+
+void InserirArea(TipoArea *Area, Aluno *UltLido, int *NRArea){
+    InsereItem(UltLido, Area);
+    *NRArea = ObterNumCelOcupadas(Area);
+}
+
+void EscreveMax(FILE **ArqLEs, Aluno R, int *Es){
+    fseek(*ArqLEs, (*Es-1) * sizeof(Aluno), SEEK_SET);
+    fwrite(&R, sizeof(Aluno), 1, *ArqLEs);
+    (*Es)--;
+}
+
+void EscreveMin(FILE **ArqEi, Aluno R, int *Ei){
+    fwrite(&R, sizeof(Aluno), 1, *ArqEi);
+    (*Ei)++;
+}
+
+void RetiraMax(TipoArea *Area, Aluno *R, int *NRArea){
+    RetiraUltimo(Area, R);
+    *NRArea = ObterNumCelOcupadas(Area);
+}
+
+void RetiraMin(TipoArea *Area, Aluno *R, int *NRArea){
+    RetiraPrimeiro(Area, R);
+    *NRArea = ObterNumCelOcupadas(Area);
+}
+
+void Particao(FILE **ArqLi, FILE **ArqEi, FILE **ArqLEs, TipoArea Area, int Esq, int Dir, int *i, int *j){
+    int Ls = Dir, Es = Dir, Li = Esq, Ei = Esq, NRArea = 0;
+    double Linf = INT_MIN, Lsup = INT_MAX;
+
+    short OndeLer = TRUE;
+    Aluno UltLido, R;
+
+    fseek(*ArqLi, (Li-1) * sizeof(Aluno), SEEK_SET);
+    fseek(*ArqEi, (Ei-1)* sizeof(Aluno), SEEK_SET);
+
+    *i = Esq-1;
+    *j = Dir+1;
+
+    while(Ls >= Li){
+        if(NRArea < TAMAREA-1){
+            if(OndeLer)
+            LeSup(ArqLEs, &UltLido, &Ls, &OndeLer);
+            else LeInf(ArqLi, &UltLido, &Li, &OndeLer);
+            InserirArea(&Area, &UltLido, &NRArea);
+            continue;
+        }
+
+        if(Ls == Es)
+        LeSup(ArqLEs, &UltLido, &Ls, &OndeLer);
+        else if(Li == Ei) LeInf(ArqLi, &UltLido, &Li, &OndeLer);
+            else if(OndeLer) LeSup(ArqLEs, &UltLido, &Ls, &OndeLer);
+                else LeInf(ArqLi, &UltLido, &Li, &OndeLer);
+        
+        if(UltLido.nota > Lsup){
+            *j = Es;
+            EscreveMax(ArqLEs, UltLido, &Es);
+            continue;
+        }
+
+        if(UltLido.nota < Linf){
+            *i = Ei;
+            EscreveMin(ArqEi, UltLido, &Ei);
+            continue;
+        }
+
+        InserirArea(&Area, &UltLido, &NRArea);
+
+        if(Ei-Esq < Dir-Es){
+            RetiraMin(&Area, &R, &NRArea);
+            EscreveMin(ArqEi, R, &Ei);
+            Linf = R.nota;
+        }
+        else{
+            RetiraMax(&Area, &R, &NRArea);
+            EscreveMax(ArqLEs, R, &Es);
+            Lsup = R.nota;
+        }
+    }
+
+    while(Ei <= Es){
+        RetiraMin(&Area, &R, &NRArea);
+        EscreveMin(ArqEi, R, &Ei);
+    }
+}
+
+void FAVazia(TipoArea *Area){
+    for(int i = 0; i < TAMAREA; i++){
+        Area->vetor[i].nota = INT_MAX;
+    }
+    Area->tam = 0;
+}
+
+void InsereItem(Aluno *UltLido,TipoArea *Area){
+    Area->vetor[Area->tam] = *UltLido;
+    Area->tam = Area->tam + 1;
+
+    insert_sort(Area);
+}
+
+int ObterNumCelOcupadas(TipoArea *Area){
+    return Area->tam;
+}
+
+void RetiraUltimo(TipoArea *Area, Aluno *R){
+    if(Area->tam == 0){
+        printf("ERRO retira ultimo 0\n");
+        return;
+    }
+
+    *R = Area->vetor[Area->tam - 1];
+    Area->vetor[Area->tam - 1].nota = INT_MAX;
+
+    insert_sort(Area);
+
+    Area->tam = Area->tam - 1;
+}
+
+void RetiraPrimeiro(TipoArea *Area, Aluno *R){
+    if(Area->tam == 0){
+        printf("ERRO retira primeiro 0\n");
+        return;
+    }
+
+    *R= Area->vetor[0];
+    Area->vetor[0].nota = INT_MAX;
+
+    insert_sort(Area);
+
+    Area->tam = Area->tam - 1;
+}
+
+void insert_sort(TipoArea *area){
+
+    Aluno aux;
+    int j, i;
+    for(i = 1; i < TAMAREA; i++){
+        aux = area->vetor[i];
+        j = i-1;
+
+        while((j >= 0) && (aux.nota < area->vetor[j].nota)){
+            area->vetor[j+1] = area->vetor[j];
+            j--;
+        }
+
+        area->vetor[j+1] = aux;
+    }
+}
---
+++ quick_sort.h
@@ -0,0 +1,47 @@
+#ifndef QUICK_SORT_H
+#define QUICK_SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "imprime.h"
+
+#define TAMAREA 20
+#define FALSE 0
+#define TRUE 1
+
+typedef struct{
+    int tam;
+    Aluno vetor[TAMAREA];
+}TipoArea;
+
+void QuicksortExterno(FILE **ArqLi, FILE **ArqEi, FILE **ArqLEs, int Esq, int Dir);
+
+void LeSup(FILE **ArqLEs, Aluno *UltLido, int *Ls, short *OndeLer);
+
+void LeInf(FILE **ArqLi, Aluno *UltLido, int *Li, short *OndeLer);
+
+void InserirArea(TipoArea *Area, Aluno *UltLido, int *NRArea);
+
+void EscreveMax(FILE **ArqLEs, Aluno R, int *Es);
+
+void EscreveMin(FILE **ArqEi, Aluno R, int *Ei);
+
+void RetiraMax(TipoArea *Area, Aluno *R, int *NRArea);
+
+void RetiraMin(TipoArea *Area, Aluno *R, int *NRArea);
+
+void Particao(FILE **ArqLi, FILE **ArqEi, FILE **ArqLEs, TipoArea Area, int Esq, int Dir, int *i, int *j);
+
+void insert_sort(TipoArea *area);
+
+void FAVazia(TipoArea *Area);
+
+void InsereItem(Aluno *UltLido,TipoArea *Area);
+
+int ObterNumCelOcupadas(TipoArea *Area);
+
+void RetiraUltimo(TipoArea *Area, Aluno *R);
+
+void RetiraPrimeiro(TipoArea *Area, Aluno *R);
+
+#endif
